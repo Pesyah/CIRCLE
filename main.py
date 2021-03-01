94aa57ffddfddc1887218a94eb0b3a11166afdda
Artur <xxarturkrossxx@yandex.ru>
2021-03-01 20:50:49 +0300

    123

--- main.py
+++ main.py
@@ -1,72 +1,32 @@
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication
+from PyQt5 import uic
+
+import sqlite3
 import sys
 
-from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from random import randint
 
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(280, 200, 220, 150))
-        self.pushButton.setObjectName("pushButton")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "Press"))
-
-
-class PaintFlag(QMainWindow, Ui_MainWindow):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.flag = False
-        self.q = 10
-        self.initUI()
+        uic.loadUi("main.ui", self)
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.pushButton.clicked.connect(self.update_result)
 
-    def initUI(self):
-        self.setupUi(self)
-        self.pushButton.clicked.connect(self.change_flag)
+    def update_result(self):
+        cur = self.con.cursor()
+        result = cur.execute("SELECT * FROM coffee", ).fetchall()
+        self.tableWidget.setRowCount(len(result))
 
-    def paintEvent(self, event):
-        if self.flag:
-            qp = QPainter()
-            qp.begin(self)
-            self.draw_flag(qp)
-            qp.end()
-
-    def change_flag(self):
-        self.flag = True
-        self.repaint()
-
-    def draw_flag(self, qp):
-        for i in range(int(self.q)):
-            r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
-            x = randint(0, 600)
-            y = randint(0, 800)
-            r = randint(0, 200)
-            qp.setBrush(QColor(r, g, b))
-            qp.drawEllipse(x, y, r, r)
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = PaintFlag()
+    ex = MyWidget()
     ex.show()
     sys.exit(app.exec())
